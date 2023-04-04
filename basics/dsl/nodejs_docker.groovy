715b9242e937c2fd63c047b651ccf380a6865ede
dvirlabs <dvirlabs@gmail.com>
2023-04-04 17:10:01 +0000

    test push to dockerhub

--- basics/dsl/nodejs_docker.groovy
+++ basics/dsl/nodejs_docker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/dvirlabs/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@devophift.work')
         }
@@ -12,9 +12,9 @@ job('NodeJS Docker example') {
     
     steps {
         dockerBuildAndPublish {
-            repositoryName('yanivomc/amdocsapp')
+            repositoryName('dvirlabs/jenkins-lab')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('yaniv-dockerhub')
+            registryCredentials('DockerHub_Auth')
             buildContext('./basics/')
             forcePull(false)
             forceTag(false)
